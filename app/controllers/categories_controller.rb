611316750dba2b1fb7266fb14b92cc2f1d044458
Sai Chaitanya <neelapu.reddy@housing.com>
2022-07-18 18:28:40 +0530

    Complete Assign App

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -18,12 +18,25 @@ class CategoriesController < ApplicationController
 
     def show 
         @category = Category.find(params[:id])
+        @articles = @category.articles.paginate(page: params[:page],per_page: 5)
     end
 
     def index
         @categories=Category.all 
     end
 
+    def edit 
+        @category = Category.find(params[:id])
+    end
+    def update 
+        @category = Category.find(params[:id])
+        if @category.update(category_params)
+            flash[:notice] ="Category name updated successfully"
+            redirect_to @category
+        else
+            render 'edit'
+        end
+    end
     private 
         def category_params
             params.require(:category).permit(:name)
